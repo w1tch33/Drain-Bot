bf69118cb513752e6cbbe8df83846b6dc69f1e60
witch <haza.ob@gmail.com>
2026-04-16 11:46:55 +1000

    Use provided Dr41n hump overlay

--- static/app.js
+++ static/app.js
@@ -139,6 +139,7 @@
     dr41nBackground.decoding = "async";
     const referenceImages = [
       "/static/themes/Dr41n-assets/Background.webp",
+      "/static/themes/Dr41n-assets/Dr41n%20hump%20overlay.png",
       "/static/themes/blood-moon-assets/Background.webp",
       "/static/themes/blood-moon-assets/Frame.webp",
     ];
